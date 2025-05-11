430adcc2680ad75110050db672da181edcd45c7b
BinalPatadia <binal.aspire@gmail.com>
2025-05-11 11:52:36 +0530

    task 11th may 5 table using printf

---
+++ src/Ex01_JavaBasics/Ex02_Task5table.java
@@ -0,0 +1,23 @@
+package Ex01_JavaBasics;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Ex02_Task5table {
+    public static void main(String[] args) {
+        int num=5;
+      char new_line='\n';
+
+        System.out.printf("%d*1",num);
+        System.out.printf(new_line+"%d*2",num);
+        System.out.printf(new_line+"%d*3",num);
+        System.out.printf(new_line+"%d*4",num);
+        System.out.printf(new_line+"%d*5",num);
+        System.out.printf(new_line+"%d*6",num);
+        System.out.printf(new_line+"%d*7",num);
+        System.out.printf(new_line+"%d*8",num);
+        System.out.printf(new_line+"%d*9",num);
+        System.out.printf(new_line+"%d*10",num);
+    }
+
+
+}
