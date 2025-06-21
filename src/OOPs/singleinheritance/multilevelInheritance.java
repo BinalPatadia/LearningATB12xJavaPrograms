5879483938bb89f21aaba6abc1b1bfe36e26793a
BinalPatadia <binal.aspire@gmail.com>
2025-06-21 12:04:10 +0530

    task 29th may 5 table using printf

---
+++ src/OOPs/singleinheritance/multilevelInheritance.java
@@ -0,0 +1,6 @@
+package OOPs.singleinheritance;
+
+public class multilevelInheritance {
+
+
+}
---
+++ src/TestingAcademy_JavaTest/Print_AllPrimitivedatatypes.java
@@ -0,0 +1,22 @@
+package TestingAcademy_JavaTest;
+
+import Ex01_JavaBasics.string;
+import org.w3c.dom.ls.LSOutput;
+
+public class Print_AllPrimitivedatatypes {
+
+    public static void main(String[] args) {
+        int i=5;
+        double d=3.14;
+        float f=1.5f;
+        long l=123456L;
+        char a='A';
+
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(l);
+        System.out.println(a);
+
+    }
+}
